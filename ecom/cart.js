39ca4afd1d73205a56ca5debf2c9d1a64e0576b0
yeasin-hossaom <yeasinhossain475@gmail.com>
2021-01-22 16:21:35 +0600

    working on total price

--- ecom/cart.js
+++ ecom/cart.js
@@ -1,13 +1,16 @@
 let cartItems = JSON.parse(localStorage.getItem('cartItem'));
 let cartBody = document.querySelector('.cart-body');
+let totalPrice = [];
 cartItems
 	.map((cartItem) => {
 		fetch(`https://fakestoreapi.com/products/${cartItem}`)
 			.then((res) => res.json())
 			.then((item) => {
 				// console.log(item.title);
+				totalPrice.push(item.price * 5);
+				totalSum(item.price, 3);
 				let items = `
-			<div class="cart-item flex justify-between my-5">
+			<div class="cart-item flex justify-between my-5 bg-white">
 			<div class="cart-img w-20">
 				<img
 					src="${item.image}"
@@ -21,7 +24,7 @@ cartItems
 			<div class="cart-quantity flex">
 				<div class="cart-down">
 					<svg
-						class="h-8"
+						class="h-8 cursor-pointer"
 						xmlns="http://www.w3.org/2000/svg"
 						viewBox="0 0 20 20"
 						fill="currentColor"
@@ -36,7 +39,7 @@ cartItems
 				<div class="quantity">2</div>
 				<div class="cart-up">
 					<svg
-						class="h-8"
+						class="h-8 cursor-pointer"
 						xmlns="http://www.w3.org/2000/svg"
 						viewBox="0 0 20 20"
 						fill="currentColor"
@@ -53,5 +56,16 @@ cartItems
 			`;
 				cartBody.insertAdjacentHTML('afterbegin', items);
 			});
+		let price = 0;
+		totalPrice.map((e) => {
+			price = price + e;
+			console.log(price);
+		});
 	})
 	.join(' ');
+
+const totalSum = async (e, b) => {
+	// return await (sum = b * e);
+	// totalPrice.push((sum = b * e));
+	// console.log(b * e);
+};
