a92a7452eea6f8a779fa4a48e7c58cac6d430336
yeasin-hossaom <yeasinhossain475@gmail.com>
2021-01-22 15:30:06 +0600

    add cart item

--- ecom/app.js
+++ ecom/app.js
@@ -9,20 +9,64 @@ const fetchAllProduct = () => {
 			let allProduct = json
 				.map((product) => {
 					return `
-                <div class="col-md-3">
-                    <div class="card product" style="width: 18rem">
-                        <img src="${product.image}"   class="card-img-top" alt="..." />
-                        <div class="card-body">
-                            <h5 class="card-title">${product.title}</h5>
-                            <h5 class="card-title">${product.price}</h5>
-                            <p class="card-text">
-                                ${product.description}
-                            </p>
-                            <a onclick="buyNow('${product.id}')" class="btn btn-primary">Buy Now</a>
-                        </div>
-                    </div>
-                </div>
-                `;
+					<div class="flex p-6 bg-gray-50 m-2 rounded-md max-w-xl">
+					  <div class="flex-none w-44 relative">
+						<img src="${product.image}" />
+					  </div>
+					  <form class="flex-auto pl-6">
+						<div class="flex flex-wrap items-baseline">
+						  <h1 class="w-full flex-none font-semibold mb-2.5">
+						  ${product.title}
+						  </h1>
+						  <div class="text-4xl leading-7 font-bold text-purple-600">$
+						  ${product.price}
+						  </div>
+						  <div class="text-sm font-medium text-gray-400 ml-3">
+							In stock
+						  </div>
+						</div>
+						<div class="flex items-baseline my-8">
+						  <div class="space-x-2 flex text-sm font-medium">
+							<label>
+							  <input class="w-9 h-9 flex items-center justify-center rounded-full bg-purple-700 text-white" name="size" type="radio" value="xs" checked>
+							  XS
+							</label>
+							<label>
+							  <input class="w-9 h-9 flex items-center justify-center rounded-full border-2 border-gray-200" name="size" type="radio" value="s">
+							  S
+							</label>
+							<label>
+							  <input class="w-9 h-9 flex items-center justify-center rounded-full border-2 border-gray-200" name="size" type="radio" value="m">
+							  M
+							</label>
+							<label>
+							  <input class="w-9 h-9 flex items-center justify-center rounded-full border-2 border-gray-200" name="size" type="radio" value="l">
+							  L
+							</label>
+							<label>
+							  <input class="w-9 h-9 flex items-center justify-center rounded-full border-2 border-gray-200" name="size" type="radio" value="xl">
+							  XL
+							</label>
+						  </div>
+						  <div class="ml-3 text-sm text-gray-500 underline">Size Guide</div>
+						</div>
+						<div class="flex space-x-3 mb-4 text-sm font-semibold">
+						  <div class="flex-auto flex space-x-3">
+							<button class="w-1/2 flex items-center justify-center rounded-full bg-purple-700 text-white" type="button">Buy now</button>
+							<button onclick="buyNow('${product.id}')"  class="w-1/2 flex items-center justify-center rounded-full bg-purple-50 text-purple-700" type="button">Add to bag</button>
+						  </div>
+						  <button class="flex-none flex items-center justify-center w-9 h-9 rounded-full bg-purple-50 text-purple-700" type="button" aria-label="like">
+							<svg width="20" height="20" fill="currentColor">
+							  <path fill-rule="evenodd" clip-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" />
+							</svg>
+						  </button>
+						</div>
+						<p class="text-sm text-gray-500">
+						  Free shipping on all continental US orders.
+						</p>
+					  </form>
+					</div>
+					`;
 				})
 				.join('');
 			products.insertAdjacentHTML('afterbegin', allProduct);
@@ -34,8 +78,25 @@ let cartIteam = [];
 const buyNow = (e) => {
 	if (cartIteam.indexOf(e) == -1) {
 		cartIteam.push(e);
+		localStorage.setItem('cartItem', JSON.stringify(cartIteam));
 		cartlist.innerText = cartIteam.length;
 	} else {
 		alert('this iteam already in your cart');
 	}
 };
+
+// `
+//                 <div class="col-md-3">
+//                     <div class="card product" style="width: 18rem">
+//                         <img src="${product.image}"   class="card-img-top" alt="..." />
+//                         <div class="card-body">
+//                             <h5 class="card-title">${product.title}</h5>
+//                             <h5 class="card-title">${product.price}</h5>
+//                             <p class="card-text">
+//                                 ${product.description}
+//                             </p>
+//                             <a onclick="buyNow('${product.id}')" class="btn btn-primary">Buy Now</a>
+//                         </div>
+//                     </div>
+//                 </div>
+//                 `
--- ecom/cart.js
+++ ecom/cart.js
@@ -1,3 +1,57 @@
-var msg = require('./Messages.js');
-
-console.log(msg);
+let cartItems = JSON.parse(localStorage.getItem('cartItem'));
+let cartBody = document.querySelector('.cart-body');
+cartItems
+	.map((cartItem) => {
+		fetch(`https://fakestoreapi.com/products/${cartItem}`)
+			.then((res) => res.json())
+			.then((item) => {
+				// console.log(item.title);
+				let items = `
+			<div class="cart-item flex justify-between my-5">
+			<div class="cart-img w-20">
+				<img
+					src="${item.image}"
+					alt=""
+				/>
+			</div>
+			<div class="cart-info">
+				<h3>${item.title}</h3>
+				<p>$ ${item.price}</p>
+			</div>
+			<div class="cart-quantity flex">
+				<div class="cart-down">
+					<svg
+						class="h-8"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M14.707 12.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L9 14.586V3a1 1 0 012 0v11.586l2.293-2.293a1 1 0 011.414 0z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+				</div>
+				<div class="quantity">2</div>
+				<div class="cart-up">
+					<svg
+						class="h-8"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M3.293 9.707a1 1 0 010-1.414l6-6a1 1 0 011.414 0l6 6a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L4.707 9.707a1 1 0 01-1.414 0z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+				</div>
+			</div>
+		</div>
+			`;
+				cartBody.insertAdjacentHTML('afterbegin', items);
+			});
+	})
+	.join(' ');
